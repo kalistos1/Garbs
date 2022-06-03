f884995e6604a62ef6d5642a74eb8c366e8a4ff7
kalistos1 <ucktech1@gmil.com>
2022-06-03 13:03:09 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { button } from 'react-bootstrap/Button'
+function App() {
+  return (
+    <>
+      <div className="bg-light mt-5 text-center">
+        <button type="button" className="btn btn-danger"> Hello world</button>
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
